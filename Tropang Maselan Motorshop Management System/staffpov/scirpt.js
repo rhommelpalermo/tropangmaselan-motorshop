5d264536fd2d56b8c6fa71691b8474b47f488253
rhommelpalermo <119090520+rhommelpalermo@users.noreply.github.com>
2025-10-26 13:42:11 +0800

    Add files via upload

---
+++ Tropang Maselan Motorshop Management System/staffpov/scirpt.js
@@ -0,0 +1,9 @@
+// Tutorial button alert
+document.querySelector(".tutorial-btn").addEventListener("click", () => {
+  alert("Tutorial will be available soon!");
+});
+
+// Sign-in button alert
+document.querySelector(".signin-btn").addEventListener("click", () => {
+  alert("Redirecting to sign-in page...");
+});
